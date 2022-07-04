db89eedf3b81361fd42c091ad1729a862e4bb797
Luis Benavides <luisalejobg@gmail.com>
2022-07-04 15:37:51 -0400

    ✅ adding and testing InMemoryRepository

---
+++ src/main/kotlin/com/example/ktodosca/repository/todo/memory/InMemoryRepository.kt
@@ -0,0 +1,31 @@
+package com.example.ktodosca.repository.todo.memory
+
+import com.example.ktodosca.domain.todo.Status
+import com.example.ktodosca.domain.todo.Todo
+import com.example.ktodosca.domain.todo.TodoId
+import com.example.ktodosca.domain.todo.repository.CreateTodo
+import com.example.ktodosca.domain.todo.repository.GetTodos
+import com.example.ktodosca.domain.todo.repository.UpdateTodo
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Repository
+
+@Repository
+@Profile("memory")
+class InMemoryRepository : CreateTodo, GetTodos, UpdateTodo {
+    private val todos: MutableList<Todo> = mutableListOf()
+
+
+    override suspend fun addTodo(todo: Todo): Todo = todo.apply { todos.add(this) }
+
+    override suspend fun all(): Flow<Todo> = todos.asFlow()
+
+    override suspend fun withStatus(status: Status): Flow<Todo> = todos.filter { it.status === status }.asFlow()
+
+    override suspend fun existsById(id: TodoId): Boolean = todos.find { it.id.value === id.value } !== null
+
+    override suspend fun update(todo: Todo): Todo = addTodo(todo)
+
+    override suspend fun remove(id: TodoId): Boolean = todos.removeIf { it.id.value === id.value }
+}
---
+++ src/test/kotlin/com/example/ktodosca/repository/todo/memory/InMemoryRepositoryTest.kt
@@ -0,0 +1,130 @@
+package com.example.ktodosca.repository.todo.memory
+
+import com.example.ktodosca.domain.todo.Status
+import com.example.ktodosca.domain.todo.Todo
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class InMemoryRepositoryTest {
+
+    private val repository: InMemoryRepository = InMemoryRepository()
+
+
+    @Test
+    fun `when a todo is added should be stored`() {
+        runBlocking {
+            val todo = Todo.fromTask("dummy")
+            val todoAdded = repository.addTodo(todo)
+            val found = repository.all().filter { it === todoAdded }.first()
+            assertEquals(todoAdded, found)
+        }
+    }
+
+    @Test
+    fun `Several values added and stored`() {
+
+        runBlocking {
+            (1..10)
+                    .map { Todo.fromTask(it.toString()) }
+                    .map { repository.addTodo(it) }
+
+
+            val filtered =
+                repository.all().toList(mutableListOf())
+
+            assertEquals(10, filtered.size)
+        }
+
+
+    }
+
+
+    @Test
+    fun `when is filter by status should return all by that status`() {
+
+        runBlocking {
+            (1..10)
+                    .map { Todo.fromTask(it.toString()).copy(status = Status.values()[it % 3]) }
+                    .map { repository.addTodo(it) }
+
+
+            val filtered =
+                repository.withStatus(Status.COMPLETE)
+                        .map { it.status }
+                        .toList(mutableListOf())
+
+            assertTrue(filtered.all { it === Status.COMPLETE }, "All the values should have the same status")
+        }
+
+
+    }
+
+    @Test
+    internal fun `when existsById is called and exists should return true`() {
+        runBlocking {
+            val fromTask = Todo.fromTask("dummy")
+
+            repository.addTodo(fromTask)
+
+            assertTrue(repository.existsById(fromTask.id))
+        }
+
+    }
+
+    @Test
+    internal fun `when existsById is called and there is not a todo with the id should return false`() {
+        runBlocking {
+            val fromTask = Todo.fromTask("dummy")
+
+            assertFalse(repository.existsById(fromTask.id))
+        }
+
+    }
+
+    @Test
+    internal fun `when remove is called and there is not a todo with the id should return false`() {
+        runBlocking {
+            val fromTask = Todo.fromTask("dummy")
+
+            assertFalse(repository.remove(fromTask.id))
+        }
+
+    }
+
+
+    @Test
+    internal fun `when remove is called and the todo exists should return true`() {
+        runBlocking {
+            val fromTask = Todo.fromTask("dummy")
+
+            repository.addTodo(fromTask)
+
+
+            assertTrue(repository.remove(fromTask.id))
+        }
+
+    }
+
+    @Test
+    internal fun `when remove is called and the todo exists should be deleted`() {
+        runBlocking {
+            val fromTask = Todo.fromTask("dummy")
+
+            repository.addTodo(fromTask)
+            repository.remove(fromTask.id)
+            assertFalse(repository.existsById(fromTask.id))
+        }
+    }
+
+    @Test
+    internal fun `when update is called should add the new value`() {
+        runBlocking {
+            val todo = Todo.fromTask("dummy")
+            val todoUpdated = repository.update(todo)
+            val found = repository.all().filter { it === todoUpdated }.first()
+            assertEquals(todoUpdated, found)
+        }
+    }
+}
