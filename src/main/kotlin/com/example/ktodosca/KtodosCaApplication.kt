8002cafa36958cd8fb70144f761d41cf02e8a601
Luis Benavides <luisalejobg@gmail.com>
2022-07-04 12:24:00 -0400

    🎉 first commit

---
+++ src/main/kotlin/com/example/ktodosca/KtodosCaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ktodosca
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtodosCaApplication
+
+fun main(args: Array<String>) {
+    runApplication<KtodosCaApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ktodosca/KtodosCaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ktodosca
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtodosCaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
