80481e816bb8846f5ca4dfb1f57d5f8e3ee4c60d
Luis Benavides <luisalejobg@gmail.com>
2022-07-05 11:16:49 -0400

    ✨ adding graphql mutations

--- src/main/kotlin/com/example/ktodosca/presentation/todo/graphql/TodoGraphql.kt
+++ src/main/kotlin/com/example/ktodosca/presentation/todo/graphql/TodoGraphql.kt
@@ -1,8 +1,13 @@
 package com.example.ktodosca.presentation.todo.graphql
 
 import com.example.ktodosca.domain.todo.Status
+import com.example.ktodosca.domain.todo.Todo
+import com.example.ktodosca.domain.todo.TodoId
+import com.example.ktodosca.usecase.todo.NewTaskAdded
+import com.example.ktodosca.usecase.todo.StatusChange
 import com.example.ktodosca.usecase.todo.TodoFetcher
 import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.CrossOrigin
@@ -10,9 +15,10 @@ import org.springframework.web.bind.annotation.CrossOrigin
 @Controller
 @CrossOrigin("*")
 class TodoGraphql(
+    private val newTask: NewTaskAdded,
+    private val statusChange: StatusChange,
     private val fetcher: TodoFetcher
                  ) {
-
 
     @QueryMapping
     suspend fun getAll() = fetcher.all()
@@ -21,4 +27,9 @@ class TodoGraphql(
     @QueryMapping
     suspend fun getByStatus(@Argument status: Status) = fetcher.byStatus(status)
 
+    @MutationMapping
+    suspend fun createTodo(@Argument task: String): Todo = newTask.createTodo(task)
+
+    @MutationMapping
+    suspend fun nextStatus(@Argument id: String) = statusChange.nextStatus(TodoId(id))
 }
